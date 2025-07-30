4b842f4952b288335f4c8255626e1596d4abd9f1
Sachin Yadav <sachin.yadav@tatvasoft.com>
2025-07-30 14:23:49 +0530

    Migration Changes

--- prisma/migrations/20250729071739_update_table/migration.sql
+++ prisma/migrations/20250729071739_update_table/migration.sql
@@ -1,14 +1,4 @@
-CREATE TYPE "PROFILE_VIEW" AS ENUM ('USER', 'PROVIDER');
-
-CREATE TYPE "RatingStatus" AS ENUM ('pending', 'approved', 'rejected');
-
 ALTER TABLE "Gig"
 ADD COLUMN "slug" TEXT NOT NULL;
-
-ALTER TABLE "User"
-ADD COLUMN "profile_view" "PROFILE_VIEW" NOT NULL DEFAULT 'USER';
-
-ALTER TABLE "ReviewRating"
-ADD COLUMN "status" "RatingStatus" NOT NULL DEFAULT 'pending';
 
 CREATE UNIQUE INDEX "Gig_slug_key" ON "Gig"("slug");
